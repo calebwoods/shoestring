3d006f77552b1122aebc03c6777ded70f90cdf9c
Caleb Woods <calebawoods@gmail.com>
2013-09-06 14:01:00 -0400

    Adding pow dns check

---
+++ lib/shoestring/pow_dns.rb
@@ -0,0 +1,25 @@
+module Shoestring
+  class PowDns < Base
+    attr_reader :port
+
+    def initalize(port = 3000)
+      @port = port
+    end
+
+    def check
+      Shoestring::Generic.check('Pow DNS') do
+        # config port for forman if installed
+        %x(if [ -f Procfile ]; then echo "port: #{port}" > .foreman; fi)
+
+        # Set up DNS through Pow
+        %x(if [ -d ~/.pow ]
+        then
+          echo #{port} > ~/.pow/`basename $PWD`
+        else
+          echo "Pow is not set up but the team uses it for this project. Setup: http://goo.gl/RaDPO"
+        fi)
+      end
+    end
+
+  end
+end
