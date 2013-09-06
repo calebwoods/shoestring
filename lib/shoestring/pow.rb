d5dd8a7be0757ca6d2676fbfd2932015e3c5c2df
Caleb Woods <calebawoods@gmail.com>
2013-09-06 17:19:10 -0400

    Add standard Pow check

---
+++ lib/shoestring/pow.rb
@@ -0,0 +1,14 @@
+module Shoestring
+  class Pow < Base
+    attr_reader :host_name
+
+    def initialize(host_name)
+      @host_name = home_name
+    end
+
+    def check
+      system("powder link #{host_name}")
+    end
+
+  end
+end
