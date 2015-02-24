49b638de09b2e0a4da88c6a8827e7e68ef864a85
Caleb Woods <calebawoods@gmail.com>
2015-02-24 12:02:04 -0500

    Refactor Homebrew check to work when Bundler loaded and Homebrew out of date

--- lib/shoestring/homebrew.rb
+++ lib/shoestring/homebrew.rb
@@ -15,7 +15,7 @@ module Shoestring
       else
         puts "You need to setup #{name} #{url}"
 
-        if system('brew -v')
+        if ::Bundler.with_clean_env { system('brew -v') }
           install_with_homebrew
         else
           install_homebrew
@@ -28,7 +28,9 @@ module Shoestring
     def install_homebrew
       puts "Homebrew is not installed. Automatically install (y/n)"
       if STDIN.gets.strip == 'y'
-        system('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"') || abort("Could not install homebrew'. Please try manually http://mxcl.github.io/homebrew/")
+        ::Bundler.with_clean_env do
+          system('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"') || abort("Could not install homebrew'. Please try manually http://mxcl.github.io/homebrew/")
+        end
       else
         abort_message
       end
@@ -37,7 +39,9 @@ module Shoestring
     def install_with_homebrew
       puts "Automatically install using homebrew (y/n)"
       if STDIN.gets.strip == 'y'
-        system("brew install #{brew_cmd}") || abort("Could not run 'brew install #{brew_cmd}'. Please try manually")
+        ::Bundler.with_clean_env do
+          system("brew update && brew install #{brew_cmd}") || abort("Could not run 'brew install #{brew_cmd}'. Please try manually")
+        end
       else
         abort_message
       end
